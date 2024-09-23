8ebd73e5f42367a24790843c6e6308c11a371b64
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-09-23 10:23:30 -0300

    Update Character.js

--- Character.js
+++ Character.js
@@ -1,15 +1,18 @@
 class Character {
-    constructor(name, lifepoints, attackPts, defensePts) {
-        this.name = name
-        this.lifepoints = lifepoints
-        this.attackPts = attackPts
-        this.defensePts = defensePts
+    constructor(name, lifepoints, attackPts, defensePts){
+        this.name=name
+        this.lifepoints=lifepoints
+        this.attackPts=attackPts
+        this.defensePts=defensePts
     }
-
     attack(target) {
-        const damage = Math.max(0, this.attackPts - target.defensePts)
+    const damage = this.attackPts - target.defensePts
+    if (damage > 0) {
         target.lifepoints -= damage
     }
-}
+    console.log(`${this.name} atacou ${target.name} e causou ${damage} pontos de dano!`)
+    console.log(`${target.name} agora tem ${target.lifepoints} pontos de vida restantes.`)
+  }
 
-module.exports = Character;
+}
+module.exports= Character
