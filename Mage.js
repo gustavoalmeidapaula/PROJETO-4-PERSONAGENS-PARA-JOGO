85cbeb87c52adc0210d1a4706366a568c300ddab
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-09-23 10:23:50 -0300

    Update Mage.js

--- Mage.js
+++ Mage.js
@@ -1,19 +1,25 @@
 const Character = require('./Character')
-
 class Mage extends Character {
     constructor(name, lifepoints, attackPts, defensePts, magicPts) {
-        super(name, lifepoints, attackPts, defensePts)
-        this.magicPts = magicPts
+        super(name, lifepoints, attackPts, defensePts);
+        this.magicPts = magicPts;
     }
 
     attack(target) {
-        const damage = Math.max(0, this.attackPts + this.magicPts - target.defensePts)
-        target.lifepoints -= damage
+        const damage = (this.attackPts + this.magicPts) - target.defensePts;
+        if (damage > 0) {
+            target.lifepoints -= damage;
+        }
+        console.log(`${this.name} atacou ${target.name} e causou ${damage} pontos de dano!`);
+        console.log(`${target.name} agora tem ${target.lifepoints} pontos de vida restantes.`);
     }
 
     heal(target) {
-        target.lifepoints += 2 * this.magicPts
+        const healAmount = 2 * this.magicPts;
+        target.lifepoints += healAmount;
+        console.log(`${this.name} curou ${target.name} em ${healAmount} pontos de vida!`);
+        console.log(`${target.name} agora tem ${target.lifepoints} pontos de vida.`);
     }
 }
 
-module.exports = Mage
+module.exports= Mage
