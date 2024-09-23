b98dd1da20fb135189b27850c19d2d36b301f04b
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-09-23 10:25:45 -0300

    Update Warrior.js

--- Warrior.js
+++ Warrior.js
@@ -1,25 +1,36 @@
-const Character = require('./Character')
+const Character =require('./Character')
 
-class Warrior extends Character {
-    constructor(name, lifepoints, attackPts, defensePts, shieldPts) {
-        super(name, lifepoints, attackPts, defensePts)
-        this.shieldPts = shieldPts
-        this.position = 'attack'
+class Warrior extends Character{
+    constructor(name, lifepoints, attackPts, defensePts, shieldPts){
+        this.name=name
+        this.lifepoints=lifepoints
+        this.attackPts=attackPts
+        this.defensePts=defensePts
+        this.shieldPts=shieldPts
+        this.position= 'defese'
     }
-
     attack(target) {
         if (this.position === 'attack') {
-            const damage = Math.max(0, this.attackPts - target.defensePts)
-            target.lifepoints -= damage
+            const damage = this.attackPts - target.defensePts;
+            if (damage > 0) {
+                target.lifepoints -= damage;
+            }
+            console.log(`${this.name} atacou ${target.name} e causou ${damage} pontos de dano!`);
         } else {
-            const damage = Math.max(0, this.attackPts + this.shieldPts - target.defensePts)
-            target.lifepoints -= damage
+            console.log(`${this.name} está em posição de defesa e não pode atacar.`);
         }
+        console.log(`${target.name} agora tem ${target.lifepoints} pontos de vida restantes.`);
     }
 
     changeStance() {
-        this.position = this.position === 'attack' ? 'defense' : 'attack'
+        if (this.position === 'attack') {
+            this.position = 'defense';
+            this.defensePts += this.shieldPts;
+        } else {
+            this.position = 'attack';
+            this.defensePts -= this.shieldPts;
+        }
+        console.log(`${this.name} mudou para a posição de ${this.position}.`);
     }
 }
-
-module.exports = Warrior
+module.exports= Warrior
