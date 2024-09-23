b4516de773ea469b3fe0f535a10282f2339e83f5
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-09-23 10:25:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,88 @@
-const Character = require('./Character')
-const Thief = require('./Thief')
+const readline = require('readline')
 const Mage = require('./Mage')
+const Character = require('./Character')
 const Warrior = require('./Warrior')
+const Thief = require('./Thief')
 
-const hero = new Character('Hero', 100, 20, 15)
+const rodada = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-const sneakyThief = new Thief('Sneaky Thief', 80, 25, 10)
+const characters = {
+    1: Character,
+    2: Mage,
+    3: Warrior,
+    4: Thief
+};
 
-const powerfulMage = new Mage('Powerful Mage', 90, 18, 12, 30)
+const characterNames = {
+    1: "Guerreiro",
+    2: "Mago",
+    3: "Guerreiro",
+    4: "Ladrão"
+}
 
-const mightyWarrior = new Warrior('Mighty Warrior', 120, 22, 20, 10)
+const players = []
 
-sneakyThief.attack(hero);
-powerfulMage.attack(mightyWarrior)
+function createPlayer(index, maxPlayers) {
+    if (index < maxPlayers) {
+        rodada.question('Escolha seu personagem (1: Guerreiro, 2: Mago, 3: Guerreiro, 4: Ladrão): ', (charChoice) => {
+            rodada.question('Digite seu apelido: ', (nickname) => {
+                rodada.question('Escolha sua arma (1: Varinha, 2: Arco, 3: Espada): ', (weaponChoice) => {
+                    const CharacterClass = characters[charChoice]
+                    const characterName = characterNames[charChoice]
+                    const weapon = weaponChoice == 1 ? 'Varinha' : weaponChoice == 2 ? 'Arco' : 'Espada'
+                    const player = new CharacterClass(nickname, 100, 20, 10, 10)
+                    player.weapon = weapon
+                    players.push(player)
 
-mightyWarrior.changeStance()
-mightyWarrior.attack(sneakyThief)
+                    console.log(`Você escolheu ser um ${characterName} com o apelido ${nickname} e usar uma ${weapon}.`)
 
-console.log(hero)
-console.log(sneakyThief)
-console.log(powerfulMage)
-console.log(mightyWarrior)
+                    createPlayer(index + 1, maxPlayers)
+                })
+            })
+        })
+    } else {
+        startGame()
+    }
+}
+
+function startGame() {
+    const monster = new Character("Monstro", 150, 25, 5)
+    let rounds = 4
+    console.log("Você entrou numa Floresta...")
+    console.log("Enquanto você anda, o chão começou a tremer...")
+    console.log("Um monstro apareceu! Lute por sua vida!!!")
+
+    function battleRound() {
+        if (rounds > 0 && monster.lifepoints > 0) {
+            players.forEach(player => {
+                player.attack(monster)
+                if (player instanceof Mage) {
+                    player.heal(player)
+                }
+            })
+
+            console.log(`O monstro agora tem ${monster.lifepoints} pontos de vida restantes! Derote-o!`)
+            rounds--
+
+            if (monster.lifepoints > 0 && rounds > 0) {
+                setTimeout(battleRound, 1000)
+            } else {
+                if (monster.lifepoints <= 0) {
+                    console.log("Parabéns! Você derrotou o monstro!")
+                } else {
+                    console.log("O monstro sobreviveu. Tente novamente!")
+                }
+                rodada.close()
+            }
+        }
+    }
+
+    battleRound()
+}
+
+rodada.question('Quantos jogadores irão participar? ', (numPlayers) => {
+    createPlayer(0, parseInt(numPlayers))
+})
